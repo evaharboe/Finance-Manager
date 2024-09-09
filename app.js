aa3483f3b87b5d123546ec7df9d2b4704851d9f9
Eva Harboe <evharboe@gmail.com>
2024-09-09 14:02:14 +0100

    progress

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const app = Vue.createApp({
         // return data object
         return {
             greeting: 'Good Morning',
+            transactions: [
+                { price: 16, category: 'Food', title: 'Lunch', isIncome: true },
+                { price: 5, category: 'Transport', title: 'Bus', isIncome: false },
+                { price: 14, category: 'Food', title: 'Lunch', isIncome: true },
+                { price: 6, category: 'Transport', title: 'Bus', isIncome: false },
+            ],
             name: 'John Doe',
-            age: 20,
-            showAge: true
+            showForm: true,
+            balance: 2000
         }
     },
     methods: {
@@ -17,9 +23,10 @@ const app = Vue.createApp({
             // this references component instance
             this.greeting = this.greeting === 'Good Morning' ? 'Good Afternoon' : this.greeting === 'Good Afternoon' ? 'Good Evening' : 'Good Morning'
         },
-        changeName(name) {
-            this.name = name
-        }
+
+        toggleShowForm() {
+            this.showForm = !this.showForm
+        },
     }
 })
 
